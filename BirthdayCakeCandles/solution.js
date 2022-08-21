d4f9f0a7b2477ae7ab46d184c386d67b9a12f2e1
Rafael Cesana <63931668+RafaCesana@users.noreply.github.com>
2022-08-20 21:24:21 -0300

    Update solution.js

--- BirthdayCakeCandles/solution.js
+++ BirthdayCakeCandles/solution.js
@@ -1,6 +1,21 @@
 function birthdayCakeCandles()
 {
-    
+    const arr = [3, 9, 9, 2, 1, 3];
+
+    // Putting array in decreasing order
+    var arrDecrOrder = arr.sort( (a, b) => b - a );
+    var MaxElement = arrDecrOrder[0];
+
+    var n = 0;
+    for( var i=0 ; i<arr.length ; i++ )
+    {
+        if( MaxElement == arrDecrOrder[i] )
+        {
+            n = n + 1;
+        }
+    }
+    R = n;
+
     // Function Return (origginaly the arr is the function parameter and a new array is returned)
-    document.getElementById('tag-p').innerHTML = "R";
+    document.getElementById('tag-p').innerHTML = R;
 }
