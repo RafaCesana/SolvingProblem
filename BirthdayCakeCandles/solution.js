6d6e9fcd3978d04c2c0c9e6b6086e1081a29e9b1
Rafael Cesana <63931668+RafaCesana@users.noreply.github.com>
2022-08-20 21:00:01 -0300

    Created a new project

---
+++ BirthdayCakeCandles/solution.js
@@ -0,0 +1,6 @@
+function birthdayCakeCandles()
+{
+    
+    // Function Return (origginaly the arr is the function parameter and a new array is returned)
+    document.getElementById('tag-p').innerHTML = "R";
+}
