c46002140ac89a49b41373324cec5d3eff3fac16
Rafael Cesana <63931668+RafaCesana@users.noreply.github.com>
2022-08-21 17:00:11 -0300

    done

--- TimeConversion/solution.js
+++ TimeConversion/solution.js
@@ -1,7 +1,49 @@
 function timeConversion()
 {
-    var R = 'test';
+    const s = '12:59:59PM';
 
+    // Separating time parts
+    var timeTermination = s.substring(s.length - 2);
+    var time = s.replace(timeTermination,'',s);
+    var hour = time.split(':',1);
+    var R = '';
+    
+    if( timeTermination == 'PM' )
+    {
+        // Eliminating 0 from 01 to 09.
+        var firstCaractere = time.substring(0,1);
+        if( firstCaractere == '0' )
+        {
+            time = time.replace(firstCaractere,'',time);
+        }
+
+        hour = time.split(':',1);
+        var n = 12;
+        for( var i=1 ; i<13 ; i++ )
+        {
+            if( hour == i )
+            {
+                n = n + i;
+                if( n == 24 )
+                {
+                    n = 12;
+                }
+            }
+        }
+        R = time.replace(hour, n, time);
+    }
+    else
+    {
+        if( hour == 12 )
+        {
+            R = time.replace(hour, '00', time);
+        }
+        else
+        {
+            R = time;
+        }
+    }
+    
     // Function Return
     document.getElementById('tag-p').innerHTML = R;
 }
