1e95a74041d56deadda4121a141c15a72423f09c
Rafael Cesana <63931668+RafaCesana@users.noreply.github.com>
2022-08-26 16:35:27 -0300

    finished

--- SequenceEquation/solution.js
+++ SequenceEquation/solution.js
@@ -1,8 +1,22 @@
 function permutationEquation()
 {
     const p = [5, 2, 1, 3, 4]
-
-    R = p
+    
+    // Adding 0 at the beginning of array
+    p.unshift(0)
+    
+    var R = []
+    var y = 0
+    for(var i=1 ; i<p.length+1 ; i++)
+    {
+        for( var y=0 ; y<p.length ; y++ )
+        {
+            if( p[p[y]] == i )
+            {
+                R.push(y)
+            }
+        }
+    }
 
     // Function Return
     document.getElementById('tag-p').innerHTML = R
