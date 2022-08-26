0491b3b6213a7b657b961781128baf77e1e13dab
Rafael Cesana <63931668+RafaCesana@users.noreply.github.com>
2022-08-26 16:37:34 -0300

    Update solution.js

--- SequenceEquation/solution.js
+++ SequenceEquation/solution.js
@@ -1,10 +1,10 @@
 function permutationEquation()
 {
     const p = [5, 2, 1, 3, 4]
-    
+
     // Adding 0 at the beginning of array
     p.unshift(0)
-    
+
     var R = []
     var y = 0
     for(var i=1 ; i<p.length+1 ; i++)
