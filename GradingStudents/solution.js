e2d362674b25e52fc2af6957142a89c7e463dad8
Rafael Cesana <63931668+RafaCesana@users.noreply.github.com>
2022-08-30 11:41:46 -0300

    Finished

--- GradingStudents/solution.js
+++ GradingStudents/solution.js
@@ -1,5 +1,28 @@
 function gradingStudents()
 {
+    // Function Parameter
+    const grades = [73, 67, 38, 33];
+    var R = '';
+
+    for( var i=0 ; i<grades.length ; i+=1 )
+    {
+        if( grades[i] >= 38 )
+        {
+            let RoundedNumber = _round(grades[i]);
+            if( (RoundedNumber - grades[i]) < 3 )
+            {
+                grades[i] = RoundedNumber;
+            }
+        }
+    }
+
+    R = grades;
+     
+    function _round(n)
+    {
+        let r = Math.ceil(n / 5) * 5;
+        return r;
+    }
 
     // Function Return
     document.getElementById('tag-p').innerHTML = R;
