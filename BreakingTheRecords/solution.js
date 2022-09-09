434dcc3b52e5679e0dcc574a6db9cee222271a84
Rafael Cesana <63931668+RafaCesana@users.noreply.github.com>
2022-09-09 17:04:46 -0300

    finished

--- BreakingTheRecords/solution.js
+++ BreakingTheRecords/solution.js
@@ -1,7 +1,30 @@
 function breakingRecords()
 {
-    R = 'test';
+    const scores = [10, 5, 20, 20, 4, 5, 2, 25, 1];
+    var R = [];
+    var n = scores[0];
+    var m = scores[0];
+    var NumOfTimesBreakingRecord = 0;
+    var NumOfTimesBreakingLeastRecord = 0;
+
+    for( var i=0 ; i<scores.length ; i++ )
+    {
+        if( scores[i] > n )
+        {
+            NumOfTimesBreakingRecord++;
+            n = scores[i];
+        }
+        if( scores[i] < m )
+        {
+            NumOfTimesBreakingLeastRecord++;
+            m = scores[i];
+        }
+    }
+
+    R.push(NumOfTimesBreakingRecord);
+    R.push(NumOfTimesBreakingLeastRecord);
     
     // Function Return
     document.getElementById('tag-p').innerHTML = R;
 }
+
